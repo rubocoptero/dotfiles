46b4d7b62dd0622dbd392df03e5ba4d8adaf8548
Ruben Anton <rubocoptero@gmail.com>
2016-04-15 10:03:35 +0000

    Change scripts to change zsh theme

---
+++ reset-zsh-theme.sh
@@ -0,0 +1,2 @@
+# Reset the oh my zsh default theme.
+sed -i 's/ZSH_THEME="gianu"/ZSH_THEME="robbyrussell"/g' ~/dotfiles/zshrc
--- use-vagrant-zsh-theme.sh
+++ use-vagrant-zsh-theme.sh
@@ -1,2 +1,2 @@
 # Change the oh my zsh default theme.
-sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="gianu"/g' ~/.zshrc
+sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="gianu"/g' ~/dotfiles/zshrc
